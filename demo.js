322438700cac1e925d10592266ed9266af911a95
Vence Lam <vence722@gmail.com>
2018-05-03 17:25:41 +0800

    add demo

--- demo.js
+++ demo.js
@@ -1,21 +1,46 @@
 const _  = require('lodash');
 const config = require('config');
-const {Producer} = require('./index');
+const {Producer, Consumer} = require('./index');
 
-async function demo() {
-	const producerConfig = _.merge({}, config.get('rabbitMQ'), {
-		queueName: 'testQueue'
+const rabbitMQConfig = _.merge({}, config.get('rabbitMQ'), {
+	queueName: 'testQueue'
+});
+
+function sleep(time) {
+	return new Promise(function (resolve) {
+		setTimeout(resolve, time);
 	});
-	const producer = new Producer(producerConfig);
+}
+
+async function demoProducer() {
+	const producer = new Producer(rabbitMQConfig);
 	try {
 		await producer.connect();
-
-		await producer.putJob("sdfsdfsdf");
+		while (true) {
+			await producer.putJob("" + new Date().getTime());
+			await sleep(100);
+		}
 	} catch (e) {
 		console.log(e);
 	}
-	console.log('3');
 	await producer.close();
 }
 
-demo();
+async function demoConsumer() {
+	const consumer = new Consumer(rabbitMQConfig);
+	try {
+		await consumer.connect();
+		while (true) {
+			const msg = await consumer.consume();
+			if (msg) {
+				console.log(`messageId: ${msg.messageId}, content: ${msg.content.toString()}`);
+			}
+		}
+	}catch (e) {
+		console.log(e);
+	}
+	await consumer.close();
+}
+
+demoProducer();
+demoConsumer();
